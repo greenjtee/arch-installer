e16156c8cd4c72a4fbbc139eaaffeef2749fb1f1
greenjtee <40438781+greenjtee@users.noreply.github.com>
2024-04-26 16:34:32 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -38,38 +38,38 @@ root_partition=$(dialog --stdout --menu "Select root partition" 0 0 0 ${partitio
 swap_partition=$(dialog --stdout --menu "Select swap partition" 0 0 0 ${partition_list}) || exit 1
 efi_partition=$(dialog --stdout --menu "Select EFI partition" 0 0 0 ${partition_list}) || exit 1
 
-mkfs.ext4 ${root_partition}
-mkswap ${swap_partition}
-mkfs.fat -F 32 ${efi_partition}
+mkfs.ext4 ${root_partition} || exit 1
+mkswap ${swap_partition} || exit 1
+mkfs.fat -F 32 ${efi_partition} || exit 1
 
-mount ${root_partition} /mnt
-mount --mkdir ${efi_partition} /mnt/boot
-swapon ${swap_partition}
+mount ${root_partition} /mnt || exit 1
+mount --mkdir ${efi_partition} /mnt/boot || exit 1
+swapon ${swap_partition} || exit 1
 
 clear
 
 ### Install and configure the basic system ###
-pacstrap -K /mnt base linux-zen linux-firmware intel-ucode man-db man-pages texinfo
+pacstrap -K /mnt base linux-zen linux-firmware intel-ucode man-db man-pages texinfo || exit 1
 
-genfstab -U /mnt >> /mnt/etc/fstab
+genfstab -U /mnt >> /mnt/etc/fstab || exit 1
 
-arch-chroot /mnt ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime
-arch-chroot /mnt hwclock --systohc
+arch-chroot /mnt ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime || exit 1
+arch-chroot /mnt hwclock --systohc || exit 1
 
-echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
-arch-chroot /mnt locale-gen
+echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf || exit 1
+arch-chroot /mnt locale-gen || exit 1
 
-echo "${hostname}" > /mnt/etc/hostname
+echo "${hostname}" > /mnt/etc/hostname || exit 1
 
-arch-chroot /mnt mkinitcpio -P
+arch-chroot /mnt mkinitcpio -P || exit 1
 
-arch-chroot /mnt passwd
+arch-chroot /mnt passwd || exit 1
 
-arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
-arch-chroot /mnt useradd ${user}
-arch-chroot /mnt passwd ${user}
+arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB || exit 1
+arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg || exit 1
+arch-chroot /mnt useradd ${user} || exit 1
+arch-chroot /mnt passwd ${user} || exit 1
 
-umount -R /mnt
+umount -R /mnt || exit 1
 
 reboot
